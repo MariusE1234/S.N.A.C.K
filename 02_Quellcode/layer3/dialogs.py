5cdb54d1e66c8a98705ca1e2ad4b0f7a43c95eb4
MariusE1234 <84963436+MariusE1234@users.noreply.github.com>
2023-04-20 21:10:41 +0200

    new class feedbackDialog

--- 02_Quellcode/layer3/dialogs.py
+++ 02_Quellcode/layer3/dialogs.py
@@ -417,6 +417,39 @@ class EditProductDialog(AddProductDialog):
         image_path = self.image_path_edit.text().strip()
         return Product(name, price, stock, image_path)
 
+class FeedbackDialog(QDialog):
+    def __init__(self, satisfaction_value, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Zufriedenheit")
+
+        layout = QVBoxLayout()
+
+        emoji_label = QLabel()
+        if satisfaction_value < 50:
+            emoji_pixmap = QPixmap("04_Images//sad.png")
+            message = "Es tut uns leid, dass dir unser Verkaufsautomat nicht gefallen hat."
+        elif 50 <= satisfaction_value < 75:
+            emoji_pixmap = QPixmap("04_Images//neutral.png")
+            message = "Danke für dein Feedback. Wir werden daran arbeiten, unseren Verkaufsautomaten zu verbessern."
+        else:
+            emoji_pixmap = QPixmap("04_Images//happy.png")
+            message = "Wir freuen uns, dass dir unser Verkaufsautomat gefällt. Danke für dein positives Feedback!"
+
+        scaled_pixmap = emoji_pixmap.scaled(100, 100, Qt.KeepAspectRatio)
+        emoji_label.setPixmap(scaled_pixmap)
+        layout.addWidget(emoji_label)
+        emoji_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(emoji_label)
+
+        message_label = QLabel(message)
+        layout.addWidget(message_label)
+
+        close_button = QPushButton("Schließen")
+        close_button.clicked.connect(self.close)
+        layout.addWidget(close_button)
+
+        self.setLayout(layout)
+
 class InfoDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -465,39 +498,7 @@ class InfoDialog(QDialog):
         self.setLayout(layout)
     
     def show_feedback(self, satisfaction_value):
-        feedback_dialog = QDialog(self)
-        feedback_dialog.setWindowTitle("Zufriedenheit")
-
-        layout = QVBoxLayout()
-
-        emoji_label = QLabel()
-        if satisfaction_value < 50:
-            emoji_pixmap = QPixmap("04_Images//sad.png")
-            message = "Es tut uns leid, dass dir unser Verkaufsautomat nicht gefallen hat."
-        elif 50 <= satisfaction_value < 75:
-            emoji_pixmap = QPixmap("04_Images//neutral.png")
-            message = "Danke für dein Feedback. Wir werden daran arbeiten, unseren Verkaufsautomaten zu verbessern."
-        else:
-            emoji_pixmap = QPixmap("04_Images//happy.png")
-            message = "Wir freuen uns, dass dir unser Verkaufsautomat gefällt. Danke für dein positives Feedback!"
-
-        # Skaliere das Bild auf die gewünschte Größe
-        scaled_pixmap = emoji_pixmap.scaled(100, 100, Qt.KeepAspectRatio)
-        emoji_label.setPixmap(scaled_pixmap)
-        layout.addWidget(emoji_label)
-        
-        # Zentriere das Bild im QLabel
-        emoji_label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(emoji_label)
-
-        message_label = QLabel(message)
-        layout.addWidget(message_label)
-
-        close_button = QPushButton("Schließen")
-        close_button.clicked.connect(feedback_dialog.close)
-        layout.addWidget(close_button)
-
-        feedback_dialog.setLayout(layout)
+        feedback_dialog = FeedbackDialog(satisfaction_value, self)
         feedback_dialog.exec()
 
 class StatDialog(QDialog):
